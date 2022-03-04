b908634211ae4c92914e91711af54f79e812cc8c
miriani0996 <badbirds09>
2022-03-04 23:29:17 +0400

    java

---
+++ gau/src/task1/Main.java
@@ -0,0 +1,25 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        /*System.out.println("""
+                C++
+                c#
+                java
+                pascal""");*/
+
+        //task2
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENter Firs Number: ");
+
+        int a = sc.nextInt();
+
+        int b = sc.nextInt();
+
+        System.out.println(a/b);
+    }
+}
+
