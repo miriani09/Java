4b53f4479a7d0d00460159c11f6eb3165d1df897
miriani0996 <badbirds09>
2022-03-25 16:15:41 +0400

    java

--- gau/src/task3/Class2_3.java
+++ gau/src/task3/Class2_3.java
@@ -1,5 +1,26 @@
 package task3;
 
-public class Class2_3 extends Class2{
+import java.util.Scanner;
 
+public class Class2_3 extends Class2{
+    int y;
+    public Class2_3(int x) {
+        super(x);
+    }
+
+    public void Method2(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter First number: ");
+        int num1 = sc.nextInt();
+        System.out.println("enter Second number: ");
+        int num2 = sc.nextInt();
+
+        x = num1;
+        y = num2;
+    }
+
+    public void Method3(){
+        System.out.println(x+y);
+    }
 }
--- gau/src/task3/Main.java
+++ gau/src/task3/Main.java
@@ -10,5 +10,8 @@ public class Main {
 
         Class2 class2 = new Class2(5);
         class2.Method1();
+        Class2_3 class2_3 = new Class2_3(6);
+        class2_3.Method2();
+        class2_3.Method3();
     }
 }
