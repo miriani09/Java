c7121e3f20c1f59392cd73a43d682098464f0388
miriani0996 <badbirds09>
2022-04-08 13:56:26 +0400

    java

--- gau/src/quiz1/Main.java
+++ gau/src/quiz1/Main.java
@@ -2,17 +2,17 @@ package quiz1;
 
 public class Main {
     public static void main(String[] args) {
-//        C c = new C();
-//        c.Method1();
-//        c.Method4();
+        C c = new C();
+        c.Method1();
+        c.Method4();
 
-//        A a = new A();
-//        a.Method1();
-//        a.Method2();
-//        a.Method3();
-//        AChild aChild = new AChild(1,2);
-//        aChild.Method4();
-//        System.out.println(aChild.Method5());
+        A a = new A();
+        a.Method1();
+        a.Method2();
+        a.Method3();
+        AChild aChild = new AChild(1,2);
+        aChild.Method4();
+        System.out.println(aChild.Method5());
 
         Task3 task3 = new Task3();
         task3.Method1();
